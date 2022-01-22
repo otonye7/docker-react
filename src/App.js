38ba80caebfde15c553372ff94c131d8e7353c58
MAC <user@MACs-MacBook-Pro.local>
2022-01-22 14:01:24 +0100

    added expose 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          NAH SORRY THAT NIGGA TERRY IS RUDE AS FUCK
+            TEST DEMO
         </p>
         <a
           className="App-link"
